6fc88cd7736879a3a2935a53b6515f6b8cf3197e
Artem <moiseev@physics.msu.ru>
2023-07-18 21:00:08 +0300

    Initial Commit

---
+++ GuesstheFlag(100SUI)/GuesstheFlag_100SUI_App.swift
@@ -0,0 +1,17 @@
+//
+//  GuesstheFlag_100SUI_App.swift
+//  GuesstheFlag(100SUI)
+//
+//  Created by Artem on 18.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct GuesstheFlag_100SUI_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
