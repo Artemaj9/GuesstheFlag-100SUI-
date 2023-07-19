853df6f7c63b3005635f557dfcb83fb6efd400c5
Artem <moiseev@physics.msu.ru>
2023-07-19 11:15:11 +0300

    Project2(100SUI) done!

--- GuesstheFlag(100SUI)/Project2.swift
+++ GuesstheFlag(100SUI)/Project2.swift
@@ -15,13 +15,15 @@ struct Project2: View {
     @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"]
     @State private var correctAnswer = Int.random(in: 0...2)
     @State private var score = 0
+    @State private var round  = 0
+    @State private var gameOver: Bool = false
     
     var body: some View {
         ZStack {
             //            LinearGradient(gradient: Gradient(colors: [.blue,.black]), startPoint: .top, endPoint: .bottom)
             RadialGradient(stops: [
                 .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
-                .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3)],
+                .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.35)],
                            center: .top, startRadius: 200, endRadius: 700).opacity(0.9)
                 .ignoresSafeArea()
             VStack {
@@ -70,6 +72,21 @@ struct Project2: View {
             } message: {
                 Text("Your score is \(score)")
             }
+            .alert(scoreTitle + "\n Game over!", isPresented: $showingScore) {
+                Button("Reset", action: reset)
+            } message: {
+                Text("Your score is \(score)")
+            }
+            .alert(scoreTitle, isPresented: $showingScore) {
+                Button("Continue", action: askQuestion)
+            } message: {
+                Text("Your score is \(score)")
+            }
+            .alert(scoreTitle + "\n Game over!", isPresented: $showingScore) {
+                Button("Reset", action: reset)
+            } message: {
+                Text("Your score is \(score)")
+            }
         }
      
      
@@ -83,15 +100,26 @@ struct Project2: View {
             scoreTitle = "Correct"
             score += 1
         } else {
-            scoreTitle = "Wrong"
+            scoreTitle = "Wrong! That's the flag of \(countries[number])!"
+            score -= 1
         }
-        showingScore = true
+        round += 1
+        if round == 8 {
+            gameOver = true
+        } else {
+            showingScore = true
+        }
     }
     
     
     func askQuestion() {
         countries.shuffle()
         correctAnswer = Int.random(in: 0...2)
+    }
+    
+    func reset() {
+        score = 0
+        askQuestion()
     }
 }
 
